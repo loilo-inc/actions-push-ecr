8c851ad51837debf08c12b563ea40a1755cd9f9a
Yusuke Sakurai <kerokerokerop@gmail.com>
2021-02-25 16:25:45 +0900

    fix: ubuntu 20.04対応 (#3)

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,12 @@ import { URL } from "url";
 
 async function main() {
   try {
-    const region =
-      core.getInput("region") ??
-      process.env["AWS_DEFAULT_REGION"]
+    const region = core.getInput("region") ??
+      process.env["AWS_DEFAULT_REGION"];
     const localImage = core.getInput("local-image");
     const pushImage = core.getInput("push-image");
     const url = new URL(`https://${pushImage}`);
-    await exec(
-      `aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${url.hostname}`
-    );
+    await loginToEcr(region, url.hostname);
     const pullImage = core.getInput("pull-image");
     if (pullImage) {
       await exec(`docker pull ${pullImage}`);
@@ -22,6 +19,33 @@ async function main() {
   } catch (e) {
     core.setFailed(e.message);
   }
+}
+
+export async function loginToEcr(region: string, repoUrl: string) {
+  console.log("logging in to ECR");
+  let result = await exec("aws", [
+    "ecr",
+    "get-login-password",
+    "--region",
+    region,
+  ]).promise();
+  if (result.code !== 0) {
+    throw new Error(result.stderr);
+  }
+  const login = await exec("docker", [
+    "login",
+    "--username",
+    "AWS",
+    "--password-stdin",
+    repoUrl,
+  ]);
+  login.p.stdin.write(result.stdout);
+  login.p.stdin.end();
+  result = await login.promise();
+  if (result.code !== 0) {
+    throw new Error(result.stderr);
+  }
+  console.log("logged in");
 }
 
 if (require.main === module) {
